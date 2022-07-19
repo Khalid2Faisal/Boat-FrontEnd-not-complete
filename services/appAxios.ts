b489ead5488457beb9dfca234ae7b86b9d376950
Khalid Faisal <Khalid.Faisal.7070@gmail.com>
2022-07-19 20:11:52 +0200

    use environment variables

--- services/appAxios.ts
+++ services/appAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://boat2boat.herokuapp.com/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     Accept: "application/json",
     "Content-type": "application/json",
